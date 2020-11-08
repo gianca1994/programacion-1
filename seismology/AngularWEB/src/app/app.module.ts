4a365e6c9c6132ed85f8c598f6ede129730ed173
Giancarlo <gianca9405@gmail.com>
2020-11-08 20:13:04 -0300

    Arreglos

--- seismology/AngularWEB/src/app/app.module.ts
+++ seismology/AngularWEB/src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SensorComponent } from './sensor/sensor/sensor.component';
-import { LoginComponent } from './login/login/login.component';
-import { UserComponent } from './user/user/user.component';
+import { SensorComponent } from './sensor/sensor.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
 import { AddNewSensorComponent } from './sensor/add-new-sensor/add-new-sensor.component';
 import { EditSensorComponent } from './sensor/edit-sensor/edit-sensor.component';
 import { DeleteSensorComponent } from './sensor/delete-sensor/delete-sensor.component';
